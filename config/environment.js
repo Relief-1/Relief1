125e0acad83f309ff95a65fbacfcb54b6cdb4bc9
Maciej Małecki <maciej.malecki@notimplemented.org>
2011-08-27 20:28:58 +0200

    New configuration options

--- config/environment.js
+++ config/environment.js
@@ -3,12 +3,27 @@ exports.common = {
     couch: {
       host: '127.0.0.1',
       port: 5984,
-      database: 'relief1'
+      database: 'relief1',
+      options: {
+        cache: true
+      }
     }
+  },
+  loginManager: {
+    hash: 'sha512'
   }
 };
 
 exports.production = {};
 exports.development = {};
-exports.test = {};
+
+exports.test = {
+  db: {
+    couch: {
+      options: {
+        cache: false
+      }
+    }
+  }
+};
 
