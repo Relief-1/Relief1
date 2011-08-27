9f3587724e09cc96a6b065c70940b6215df24f89
Maciej Małecki <maciej.malecki@notimplemented.org>
2011-08-27 02:32:13 +0200

    Configuration file

---
+++ config/environment.js
@@ -0,0 +1,14 @@
+exports.common = {
+  db: {
+    couch: {
+      host: '127.0.0.1',
+      port: 5984,
+      database: 'relief1'
+    }
+  }
+};
+
+exports.production = {};
+exports.development = {};
+exports.test = {};
+
